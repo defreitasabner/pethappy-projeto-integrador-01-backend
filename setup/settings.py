cdaf045e6efdfd5f8faf2000c6805d844ce793e8
Abner Freitas <defreitasabner@gmail.com>
2024-07-16 19:59:36 -0300

    Implementado container de desenvolvimento apenas com o banco de dados.

--- setup/settings.py
+++ setup/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pet_happy_db',
         'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
