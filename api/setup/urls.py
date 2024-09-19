5a7fdaabb346f9206a354fe9e1ff52a96e52ef42
Abner Freitas <defreitasabner@gmail.com>
2024-09-18 21:01:09 -0300

    Implementada documentação da API via Swagger.

--- api/setup/urls.py
+++ api/setup/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from apps.pessoas.urls import pessoas_router
 from apps.pets.urls import pets_router
@@ -11,10 +13,25 @@ router = routers.DefaultRouter()
 router.registry.extend(pessoas_router.registry)
 router.registry.extend(pets_router.registry)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação da Pet Happy API",
+      default_version='v1',
+      description="Backend do projeto Pet Happy",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include(router.urls)),
     path('clientes/<int:pk>/pets/', ListarPetsDoClienteView.as_view()),
 ]
