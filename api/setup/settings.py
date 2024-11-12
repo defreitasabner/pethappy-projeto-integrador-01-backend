ebc46d1077fa012fce6543b1efa4c12328a7be59
Abner Freitas <defreitasabner@gmail.com>
2024-11-12 19:05:59 -0300

    Aumentada a vida útil do access token.

--- api/setup/settings.py
+++ api/setup/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import sys
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,8 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Django Rest Framework Settings
+# Django Simple JWT Settings
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours = 1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days = 1),
+    'UPDATE_LAST_LOGIN': False,
+}
 
+# Django Rest Framework Settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -124,7 +131,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',        
         'rest_framework.filters.SearchFilter',
